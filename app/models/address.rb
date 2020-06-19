5e0afdc41493012233ebb06748ede853bed42f59
Joyce Kao <joyceykao@gmail.com>
2020-06-19 20:55:46 +0200

    added random short generator

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,14 @@
 class Address < ApplicationRecord
   validates :url, presence: true
   validates :url, format: URI.regexp(%w[http https])
-  validates :short, presence: true
+  # validates :short, presence: true
   validates :short, uniqueness: true
-  validates :short, format: { with: /\A[a-zA-Z]+\z/,
-    message: "only allows letters" }
+  validates :short, format: { with: /\A[a-zA-Z0-9]*\z/,
+    message: "only allows letters and numbers" }
+
+  before_validation :random_short
+
+  def random_short
+    self.short = (rand*100000).ceil.to_s if self.short.nil? || self.short.empty?
+  end
 end
