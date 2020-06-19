d3d9c1898f80d6ef9c02579bf78d190fcd31cea9
Joyce Kao <joyceykao@gmail.com>
2020-06-19 12:17:40 +0200

    root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'addresses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     get '/go/:short', to: 'addresses#show', as: :shorten
     resources :addresses, only: [:new, :create, :index]
