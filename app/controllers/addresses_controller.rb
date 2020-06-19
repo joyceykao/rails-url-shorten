f49336dd426183c3929f48c15ea300085d17acd5
Joyce Kao <joyceykao@gmail.com>
2020-06-19 12:00:15 +0200

    new button to add new link

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,8 +2,29 @@ class AddressesController < ApplicationController
   def index
     @addresses = Address.all
   end
+
   def show
     @address = Address.find_by_short(params[:short])
     redirect_to @address.url
+  end
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to addresses
+
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:url, :short)
   end
 end
