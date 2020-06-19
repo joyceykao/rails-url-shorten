58ae09aeb0b59d3b4889082ce33e7786ab942af7
Joyce Kao <joyceykao@gmail.com>
2020-06-19 12:28:53 +0200

    added copy and past method

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+  validates :url, presence: true
+  validates :short, uniqueness: true
+
+  def shorten
+    Rails.application.routes.url_helpers.short_url(short: self.short)
+  end
 end
