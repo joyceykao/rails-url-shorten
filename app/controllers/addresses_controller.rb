b97ca91c583903056431f53a2dc3803c787264ea
Joyce Kao <joyceykao@gmail.com>
2020-06-19 11:44:10 +0200

    basic view

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,7 @@
 class AddressesController < ApplicationController
+  def index
+    @addresses = Address.all
+  end
   def show
     @address = Address.find_by_short(params[:short])
     redirect_to @address.url
