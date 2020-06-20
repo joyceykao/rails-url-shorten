b1cc2b959a413cb34bbf10c79bc5c6929ef328a3
Joyce Kao <joyceykao@gmail.com>
2020-06-20 20:21:28 +0200

    added 404 redirect if shortened link doesn't exist.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -5,7 +5,11 @@ class AddressesController < ApplicationController
 
   def show
     @address = Address.find_by_short(params[:short])
-    redirect_to @address.url
+    if @address.nil? || @address.empty?
+      render_404
+    else
+      redirect_to @address.url
+    end
   end
 
   def new
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def render_404
+    render file: "#{Rails.root}/public/404", status: :not_found
+  end
 end
